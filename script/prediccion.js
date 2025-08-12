fd1dfe127ff3a3615a3ddbe0eb2ab4713747f9e2
Andrés Valdivieso Sorando <andres24vs@gmail.com>
2025-08-12 00:09:22 -0400

    ADD Css de prediccion y medida de media query cambiada a 600
    En predicción.js se encuentran los distintos mensajes que puede tener el texto del resultado

---
+++ script/prediccion.js
@@ -0,0 +1,5 @@
+/*
+            <h2 class="texto-resultado">Aún no se ha calculado <b>NINGUNA</b> predicción, <br> Ingrese los datos y presione <b>CALCULAR</b></h2>
+            <h2 class="texto-resultado imposible">Ya <em>NO</em> existe forma de que apruebes la materia</h2>
+            <h2 class="texto-resultado">La nota <b>MÍNIMA</b> que debes de sacar en la evaluación<br>para aprobar la materia es <b>15</b><br>Y tu nota final sería <b>10.1</b> que redondeado seria <b>10</b></h2>
+*/
